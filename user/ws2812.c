7475a95b60fea6f1d1ee88833952ec012d54f481
Gijs <g.w.neerhof@student.tue.nl>
2020-09-06 18:59:21 +0200

    changed LED code

--- user/ws2812.c
+++ user/ws2812.c
@@ -7,6 +7,8 @@
 #define GPIO_OUTPUT_SET(gpio_no, bit_value) \
 	gpio_output_set(bit_value<<gpio_no, ((~bit_value)&0x01)<<gpio_no, 1<<gpio_no,0)
 
+static uint32_t rainb12;
+rainb12=1<<2;
 
 //I just used a scope to figure out the right time periods.
 
@@ -107,4 +109,45 @@ void   WS2812OutBuffer( uint8_t * buffer, uint16_t length, int light_level )
 	}
 
 	ets_intr_unlock();
+}
+
+void ICACHE_FLASH_ATTR SEND_WS_12_0() {
+    uint8_t time;
+    time = 4;
+    while (time–)
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 4, rainb12);
+    time = 9;
+    while (time–)
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 8, rainb12);
+}
+
+void ICACHE_FLASH_ATTR SEND_WS_12_1() {
+    uint8_t time;
+    time = 8;
+    while (time–)
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 4, rainb12);
+    time = 6;
+    while (time–)
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 8, rainb12);
+}
+
+void ICACHE_FLASH_ATTR WS2812OutBuffer(uint8_t * buffer, uint16_t length, uint16_t repetition) {
+    uint16_t i;
+    ets_intr_lock();
+
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 8, rainb12);
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 8, rainb12);
+        WRITE_PERI_REG(PERIPHS_GPIO_BASEADDR + 8, rainb12);
+        while (repetition–) {
+            for (i = 0; i < length; i++) {
+                uint8_t mask = 0x80;
+                uint8_t byte = buffer[i];
+                while (mask) {
+                    (byte & mask) ? SEND_WS_12_1() : SEND_WS_12_0();
+                    mask >>= 1;
+                }
+            }
+        }
+
+    ets_intr_unlock();
 }
