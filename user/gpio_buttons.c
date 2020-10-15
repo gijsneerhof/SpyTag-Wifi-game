4178669b4f6e888c402663ea6bbbc4730a29600e
Gijs <g.w.neerhof@student.tue.nl>
2020-10-15 16:59:57 +0200

    Update gpio_buttons.c

--- user/gpio_buttons.c
+++ user/gpio_buttons.c
@@ -11,7 +11,7 @@ volatile uint8_t LastGPIOState;
 
 //static const uint8_t GPID[] = { 0, 12 };
 static const uint8_t GPID[] = {12, 4};
-static const uint8_t Func[] = {FUNC_GPIO12, FUNC_GPIO4);
+static const uint8_t Func[] = {FUNC_GPIO12, FUNC_GPIO4};
 static const int  Periphs[] = { PERIPHS_IO_MUX_MTDI_U, PERIPHS_IO_MUX_GPIO4_U};
 
 
